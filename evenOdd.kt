8ed5b7a738d299744b4a04758118d553f35bda95
khushi <khushimehta0206@gmail.com>
2024-08-23 13:22:32 +0530

    Add files via upload

---
+++ evenOdd.kt
@@ -0,0 +1,10 @@
+fun main(){
+    // check whether the number is even or odd
+    var num:Int = readln().toInt();
+    if(num%2==0){
+        println("$num is even number");
+    }
+    else{
+        println("$num is odd number");
+    }
+}
