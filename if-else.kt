8e950dae042ba2f463b55d83f34da27ebd759905
khushi <khushimehta0206@gmail.com>
2024-08-23 13:13:31 +0530

    Add files via upload

---
+++ if-else.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //conditional statesment
+    // take a variable and check it is positive negative or equals t zero..
+    var number :Int = readln().toInt();
+    if(number>0){
+        println("$number is positve");
+    }
+    else if(number==0){
+        println("Zero!!!");
+    }
+    else{
+        println("$number is negative");
+    }
+}
