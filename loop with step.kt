9343af297b9f03fbe06d55a9de918c477f8aba5f
khushi <khushimehta0206@gmail.com>
2024-08-25 15:27:14 +0530

    Add files via upload

---
+++ loop with step.kt
@@ -0,0 +1,11 @@
+fun main(){
+    //print numbers 1 to 30 having gap of 4
+    for(i in 1..30 step 4){
+        print("$i ")
+    }
+    //print in reverse order
+    println()
+    for(i in 30 downTo 1 step 4){
+        print("$i ")
+    }
+}
