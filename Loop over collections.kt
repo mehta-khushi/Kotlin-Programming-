8a238509c002da99bc93d71f03d1f0bf4c078cd6
khushi <khushimehta0206@gmail.com>
2024-08-25 15:39:47 +0530

    Add files via upload

---
+++ Loop over collections.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //iterate over a list
+    var fruits = listOf("Apple","Mango","Banana","Grapes","Pineapple")
+    for(fruit in fruits){
+        println(fruit)
+    }
+    // iterate over an array
+    var nums = arrayOf(1,2,3,4,5)
+    for(num in nums){
+        print("$num ")
+    }
+
+}
