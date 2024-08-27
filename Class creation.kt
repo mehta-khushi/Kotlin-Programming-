f492552adef188e69ae647416243e143d6fba387
khushi <khushimehta0206@gmail.com>
2024-08-27 22:13:04 +0530

    Add files via upload

---
+++ Class creation.kt
@@ -0,0 +1,10 @@
+// class is template used to initialize the objects constructor getter setter etc
+class xyz{  //class header
+    fun intro(){
+        print("hello :)")
+    }
+}
+fun main(){
+    var a = xyz()
+    a.intro()
+}
